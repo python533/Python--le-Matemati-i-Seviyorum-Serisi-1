0b0828669d0eba2f28c5b81d8df413d0239d1fcd
python533 <143389231+python533@users.noreply.github.com>
2024-04-15 10:34:11 +0300

    Project EulerSoru-1
    Project Euler 1: Eğer 10'un altında kalan ve 3 ile 5'in katları olan bütün doğal sayıları sıralarsak: 3, 5, 6 ve 9'u elde ederiz. Bu sayıların toplamı da 23'tür. 1000'den küçük 3 veya 5'e bölünen tüm sayıların toplamları nedir?

---
+++ Python İle mat/cozum.py
@@ -0,0 +1,18 @@
+class x:
+    def __init__(self,a):
+        self.a=a
+    def kontrol(self):
+        if self.a % 3 == 0 or self.a % 5 == 0:
+            return True:
+        else:
+        return False
+
+    def kontrol():
+        sum=0
+    for i in range(0,1000):
+        if kontrol(i) == True:
+            sum +=1
+
+    print(sum)
+
+
